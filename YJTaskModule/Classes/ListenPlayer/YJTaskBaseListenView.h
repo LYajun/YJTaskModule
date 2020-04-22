797545312c0a468955d150877e380eb3d41895e4
刘亚军 <liuyajun1999@icloud.com>
2020-04-22 16:22:08 +0800

    优化

---
+++ YJTaskModule/Classes/ListenPlayer/YJTaskBaseListenView.h
@@ -0,0 +1,16 @@
+//
+//  YJTaskBaseListenView.h
+//  AFNetworking
+//
+//  Created by 刘亚军 on 2020/4/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YJTaskBaseListenView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YJTaskModule/Classes/ListenPlayer/YJTaskBaseListenView.m
@@ -0,0 +1,14 @@
+//
+//  YJTaskBaseListenView.m
+//  AFNetworking
+//
+//  Created by 刘亚军 on 2020/4/22.
+//
+
+#import "YJTaskBaseListenView.h"
+
+@implementation YJTaskBaseListenView
+
+
+
+@end
